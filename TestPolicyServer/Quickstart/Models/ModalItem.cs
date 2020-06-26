b720b4150268a11f1f38e8e743bc08207eb0ce32
Steven <steven.demarco@while-not-false.ch>
2020-06-26 16:37:37 +0200

    added modal

---
+++ TestPolicyServer/Quickstart/Models/ModalItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestPolicyServer.Quickstart {
+    public class ModalItem {
+
+        public String Title { get; set; }
+        public String Caption { get; set; }
+
+        public String OkButton { get; set; } = "Ok";
+        public String CancelButton { get; set; } = "Cancel";
+
+    }
+}
